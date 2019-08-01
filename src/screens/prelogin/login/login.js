3f0c99d4316e86cf0eb5971d11a27620d457878d
PKJ94 <pradeep.94@live.com>
2019-08-01 23:35:45 +0530

    test

--- src/screens/prelogin/login/login.js
+++ src/screens/prelogin/login/login.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TextInput, Button } from 'react-native';
 import { dynamicSize } from '../../../utility/responsive';
 import Style from "./style";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            username: '',
+            password: ''
+        };
         // alert(Width)
     }
     render() {
         return (
             <View style={Style.container}>
                 <Image resizeMode="contain" style={Style.logoImage} source={require('../../../assets/image/logo.png')}></Image>
-                <Text>Hello</Text>
-                <View style={Style.bottomView}><Text>Signup</Text></View>
+                <TextInput style={Style.input} value={this.state.username} editable = {true} inlineImageLeft='search_icon'></TextInput>
+                <TextInput style={Style.input}  value={this.state.password} password editable = {true}></TextInput>
+                <View style={Style.buttonView}>
+                    <TouchableOpacity
+                        style={Style.buttonTouch}
+                        uppercase={false}
+                        block
+                    >
+                        <Text style={Style.buttonText}>Sign In</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text style={Style.forgotOuterText}>Forgotten your login detail? <Text style={Style.forgotInnerText}>Get help with Sign In</Text></Text>
+                <View style={Style.border}>
+                    <Text style={Style.borderText}>OR</Text>
+                </View>
+                <View style={Style.bottomView}>
+                    <Text style={Style.bottomViewOuterText}>Do you want an account? 
+                        <Text style={Style.bottomViewInnerText}> Signup</Text>
+                    </Text>
+                </View>
             </View>
         );
     }
--- src/screens/prelogin/login/style.js
+++ src/screens/prelogin/login/style.js
@@ -5,12 +5,74 @@ import { dynamicSize, Width } from '../../../utility/responsive';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "red",
+        backgroundColor: "white",
         justifyContent: 'center',
         alignItems: 'center'
     },
     logoImage: {
-        width: dynamicSize(Width / 1.5)
+        width: dynamicSize(Width / 2)
     },
-    bottomView: { position: 'absolute', bottom: 0, borderTopWidth: dynamicSize(1), flex: 1, width: '100%', alignItems: 'center', justifyContent: 'center' }
+    bottomView: {
+        position: 'absolute',
+        bottom: 0,
+        borderTopWidth: 2,
+        borderTopColor: '#e9e9e9',
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        padding: dynamicSize(10),
+        justifyContent: 'center'
+    },
+    bottomViewOuterText: {
+        color: '#7a7a7a',
+    },
+    bottomViewInnerText: {
+        fontWeight: '700'
+    },
+    input: {
+        borderColor: '#e9e9e9',
+        borderWidth: 1,
+        width: '80%',
+        height: dynamicSize(40),
+        marginBottom: dynamicSize(10),
+        paddingStart: dynamicSize(10),
+        paddingEnd: dynamicSize(10),
+    },
+    buttonView: {
+        width: '80%',
+        backgroundColor: "#e69d48",
+        textTransform: 'none',
+        height: dynamicSize(40),
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: dynamicSize(15)
+    },
+    forgotOuterText: {
+        marginTop: dynamicSize(10),
+        color: "#7a7a7a",
+        fontSize: dynamicSize(12)
+    },
+    forgotInnerText: {
+        fontWeight: '700',
+
+    },
+    border: {
+        borderBottomColor: '#e9e9e9',
+        borderBottomWidth: 2,
+        width: '80%',
+        alignItems: 'center',
+        marginTop: dynamicSize(20)
+    },
+    borderText: {
+        position: 'absolute',
+        color: '#7a7a7a',
+        padding: dynamicSize(10),
+        top: -23,
+        backgroundColor: 'white',
+        fontSize: dynamicSize(12)
+    }
 });
